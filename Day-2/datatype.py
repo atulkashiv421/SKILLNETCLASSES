764a324aedc54007c1fc548a07166ca96f4f06c2
Atul <ddk801973@gmail.com>
2025-07-15 18:07:25 +0530

    Add files via upload

---
+++ Day-2/datatype.py
@@ -0,0 +1,27 @@
+#about datatype 
+x=3j
+print(type(x))
+
+
+fruit=("apple",)
+print(type(fruit))
+
+x = range(6)
+print(type(x))
+
+x={"name":"pankaj","age":19}
+
+x=bytearray(5)
+print(x)
+
+#multiline string
+a = """1
+2
+3
+4
+5
+6
+7"""
+#importing
+import random 
+print(random.randrange(1,10))
